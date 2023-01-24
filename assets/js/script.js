3b7d3d8d24a13a43ecfc77aefde4e242095cd22f
Davi Costa <davim.costa@outlook.com>
2023-01-23 21:10:19 -0300

    :sparkles: alteração da cor do botão no modo dark :sparkles:

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,6 +2,7 @@ const moon = document.getElementById('mode_icon');
 
 moon.addEventListener('click', () => {
     const form = document.getElementById('login_form');
+    const button = document.getElementById('login_button');
     const container = document.getElementById('container');
     if(moon.classList.contains('fa-moon')) {
         moon.classList.remove('fa-moon');
@@ -9,12 +10,15 @@ moon.addEventListener('click', () => {
 
         form.classList.add('dark')
         container.classList.add('dark')
+        button.classList.add('dark')
         return;
     }
 
     moon.classList.add('fa-moon');
     moon.classList.remove('fa-sun');
+
     form.classList.remove('dark')
     container.classList.remove('dark')
+    button.classList.remove('dark')
 });
 
